19d227edeb1905b009ff93a77282aebcefc5d14a
Bruno Queiroz <bruno.queiroz2011@gmail.com>
2019-05-29 16:11:21 -0300

    Inclusão das rotas para os CRUD de Produtos, eventos, usuário, favoritos e cafeterias

--- Backend/api/routes/router.js
+++ Backend/api/routes/router.js
@@ -7,8 +7,16 @@ module.exports = function (server){
     const ctbCoffeShop = require('../controllers/coffeshop/coffeshopService')
     ctbCoffeShop.register(router,'/coffeshop')
 
-    
+    const ctbUser = require('../controllers/user/userService')
+    ctbUser.register(router,'/user')
 
-    
+    const ctbEvent = require('../controllers/event/eventService')
+    ctbEvent.register(router,'/event')
 
+    const ctbProduct = require('../controllers/product/productService')
+    ctbProduct.register(router,'/product')
+    
+    const ctbFavorite = require('../controllers/favorite/favoriteService')
+    ctbFavorite.register(router,'/favorite')
+    
 }
