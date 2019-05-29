b4fa5c6ee4fff5c4b4295423a6f45d4aecdaa904
Bruno Queiroz <bruno.queiroz2011@gmail.com>
2019-05-29 16:09:25 -0300

    Criação do Model para espelhar a tabela de Usuário

--- Backend/api/models/user.js
+++ Backend/api/models/user.js
@@ -1,12 +1,43 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-const userSchema = new mongoose.Schema({    
-    login:{type:String, require:[true, 'Informe o login do Usuário']},
+
+const userSchema = new mongoose.Schema({  
+    cpf:{type:Number,min:11, require:[true, "Informe o seu CPF!"], index: { unique: true }},  
+    username:{type: String, require: [true, "Informe o login da cafeteria"]},        
     password:{type:String, require:[true, 'Informe a senha do Usuário']},
     name:{type:String, require:[true, 'Informe o nome do Usuário']},
     email:{type:String, require:[true, 'Informe o e-mail do Usuário']},
-    neighborhood:{type:String, require:[true, 'Informe o bairro do Usuário']}    
+    cep:{type:Number, require:[true, 'Informe o bairro do Usuário']}    
 })
+
+userSchema.pre('save', function(next) {
+    var user = this;
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 module.exports = restful.model('user', userSchema)
