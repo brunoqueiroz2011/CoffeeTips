72d78590f5e2162a9c3e53e8e12e9b3e3adabaad
Bruno Queiroz <bruno.queiroz2011@gmail.com>
2019-06-18 10:28:34 -0300

    Ajustes na baseURL das rotas e ajuste do layout e component da home

--- Frontend/Dashboard/src/app/home/home.component.ts
+++ Frontend/Dashboard/src/app/home/home.component.ts
@@ -1,39 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { HomeService } from './home.service';
-import { Product } from 'app/product/product.model';
 
 @Component({
   selector: 'ctd-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit {  
 
-    product : number
+  constructor() {}
 
-    qtdProducts: string
-    qtdEvent: string = ""
-    qtdReport: string = ""    
-    qtdComments: string
-  
-
-  constructor(private homeService: HomeService) {    
-  }
-
-  ngOnInit() {
-    this.homeService.qtdPruducts().subscribe(product => 
-                                              product.length > 0
-                                                ? this.qtdProducts =  product.length.toString()
-                                                : this.qtdProducts = "0")
-    this.homeService.qtdEvents().subscribe(evento => 
-                                            evento.length > 0 
-                                              ? this.qtdEvent = evento.length.toString()
-                                              : this.qtdEvent = "0")
-
-    this.homeService.qtdComments().subscribe(comment => 
-                                             comment.length > 0 
-                                              ? this.qtdComments = comment.length.toString()
-                                              : this.qtdComments = "0")                                            
-  }
+  ngOnInit() {}
 
 }
